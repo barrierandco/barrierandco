3df733f9cebbee30147b2340533e492522f292ad
John Wilson <john@j-wilson.com>
2017-10-17 19:03:25 -0400

    Remove logos and references to CBT and Grooveshark

--- src/components/objections.js
+++ src/components/objections.js
@@ -22,30 +22,18 @@ const ObjectionsContainer = styled.div`
   }
 `
 
-const ObjectionsLogos = styled.img`
-  display: block;
-  height: auto;
-  margin: 24px auto 0;
-  max-width: 650px;
-  width: 100%;
-  @media ${props => props.theme.breakpoint} {
-    margin: 48px auto 0;
-  }
-`
-
 const Objections = props =>
   <div>
     <ObjectionsContainer>
       <div className="column">
         <h2>Why should you&nbsp;trust&nbsp;me?</h2>
-        <p>Since 2009, I’ve had experience working with large brands such as Cisco, Booz Allen Hamilton, Grooveshark, Cross Browser Testing and dozens of other startups. Having come through a more traditional design training and now focusing on web technologies, I have a wide range of experiences that all tie together to create a keen eye for the macro and micro scopes of&nbsp;each&nbsp;project.</p>
+        <p>Since 2009, I’ve had experience working with large brands such as Cisco, Booz Allen Hamilton and dozens of other startups. Having come through a more traditional design training and now focusing on web technologies, I have a wide range of experiences that all tie together to create a keen eye for the macro and micro scopes of&nbsp;each&nbsp;project.</p>
       </div>
       <div className="column">
         <h2>Can I <em>really</em> help&nbsp;you&nbsp;out?</h2>
         <p>I'm positive that I can, but let me put it this way… if we chat and I’m not certain that your investment will make a profitable return, or I’m not capable of helping you out, I’ll let you know up front. There’s no point in wasting either of our time or your money. In other words, I’ll only help you out if I’m certain that there is a chance for a&nbsp;positive&nbsp;result.</p>
       </div>
     </ObjectionsContainer>
-    <ObjectionsLogos src={logos} alt="a collection of logos from Grooveshark, Booz Allen Hamilton and Cross Browser Testing" />
   </div>
 
 export default Objections
