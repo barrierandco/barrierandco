168ad70ec785d6155ff41442a999ee930b885128
John Wilson <john@j-wilson.com>
2017-10-23 11:02:45 -0400

    Remove drift

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -32,28 +32,6 @@ const TemplateWrapper = ({ children }) => (
       <meta property="og:description" content="Barrier & Company exists to create a solution for some of today's greatest technological problems so that other people can enjoy the fullness of life itself." />
       <meta property="og:site_name" content="Barrier & Co." />      
       <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/3980674.js"></script>
-      <script type="text/javascript">{`
-        !function() {
-          var t;
-          if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0, 
-          t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-          t.factory = function(e) {
-            return function() {
-              var n;
-              return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
-            };
-          }, t.methods.forEach(function(e) {
-            t[e] = t.factory(e);
-          }), t.load = function(t) {
-            var e, n, o, i;
-            e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"), 
-            o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js", 
-            n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
-          });
-        }();
-        drift.SNIPPET_VERSION = '0.3.1';
-        drift.load('vtkuwry32ttp');
-      `}</script>
     </Helmet>
     <ThemeProvider theme={settings}>
       {children()}
